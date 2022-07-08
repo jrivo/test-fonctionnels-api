deccfc43e74d82c14a7ca183444e7a16c63f41a5
amin <aminwastaken@gmail.com>
2022-07-08 15:35:27 +0200

    Authorization test

--- features/category.context.js
+++ features/category.context.js
@@ -8,14 +8,30 @@ const {
 } = require("@cucumber/cucumber");
 const request = require("supertest");
 const { expect } = require("expect");
+const controller = require("../app/controllers/category.controller");
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+function generateString(length) {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+const username = generateString(12);
+const password = generateString(12);
+let token = "";
 
 Before(function () {
   this.client = request(require("../server.js"));
 });
 
-After(function () {
-  //   const { connection } = require("../models");
-  //   connection.query('DELETE FROM "Products"');
+After(async () => {
+  await controller.prisma.category.deleteMany({});
 });
 
 AfterAll(function () {
@@ -23,22 +39,53 @@ AfterAll(function () {
   //   connection.close();
 });
 
-Given("I have no resources", function () {
-  // Write code here that turns the phrase above into concrete actions
+Given("I am connected as {string}", async function (role) {
+  const response = await this.client["post"]("/register").send({
+    username: username,
+    password: password,
+    email: username + "@email.com",
+    role: role?.toUpperCase(),
+  });
+
+  const loginResponse = await this.client["post"]("/login").send({
+    username: username,
+    password: password,
+  });
+  this.token = loginResponse.body.accessToken;
 });
 
+Given("I have no resources", function () {});
+
 When("I call {string} {string}", async function (method, url) {
-  // Write code here that turns the phrase above into concrete actions
   this.response = await this.client[method.toLowerCase()](url).send();
 });
 
 Then("I should get a {int} response code", function (int) {
-  // Then('I should get a {float} response code', function (float) {
-  // Write code here that turns the phrase above into concrete actions
   expect(this.response.status).toBe(int);
 });
 
 Then("I should get an empty array", function () {
-  // Write code here that turns the phrase above into concrete actions
   expect(this.response.body).toEqual([]);
+});
+
+Given("I have a payload", function (dataTable) {
+  this.payload = dataTable.rowsHash();
+});
+
+When("I call {string} {string} with the payload", async function (method, url) {
+  this.response = await this.client[method.toLowerCase()](url)
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + this.token)
+    .send(this.payload);
+});
+
+Then(
+  "The property {string} should be present in the response",
+  function (string) {
+    expect(this.response.body[string]).toBeDefined();
+  }
+);
+
+Then("The property {string} should be {string}", function (string, string2) {
+  expect(this.response.body[string]).toBe(string2);
 });
--- features/category.feature
+++ features/category.feature
@@ -7,10 +7,11 @@ Feature: Category
         And I should get an empty array
 
 
-# Scenario: should create a product
-#     Given I have a payload
-#         | name | Category 1 |
-#     When I call "POST" "/categories" with the payload
-#     Then I should get a 201 response code
-#     And The property "id" should be present in the response
-#     And The property "name" should be "Product 1"
+    Scenario: should create a product
+        Given I am connected as "admin"
+        And I have a payload
+            | name | Category 1 |
+        When I call "POST" "/categories" with the payload
+        Then I should get a 201 response code
+        And The property "id" should be present in the response
+        And The property "name" should be "Category 1"
